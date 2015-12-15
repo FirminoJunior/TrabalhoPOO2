1d34d5cc994af422b896d2a5d4ae99c06797e906
Jose_Junior <Jose_Junior@FirminoJunior>
2015-12-15 18:28:50 -0300

    alteração cor

--- TrabalhoPOO2/src/Principal/Desenho.java
+++ TrabalhoPOO2/src/Principal/Desenho.java
@@ -1,9 +1,5 @@
 package Principal;
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 import javax.swing.JFrame;
 import java.awt.Canvas;
 import java.awt.Color;
